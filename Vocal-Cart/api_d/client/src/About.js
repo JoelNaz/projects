4dc33a38847dad2627f6f6b5963f3555653576d9
JoelNaz <113906521+JoelNaz@users.noreply.github.com>
2023-10-30 06:30:27 +0530

    Delete Vocal-Cart/api_d directory

--- Vocal-Cart/api_d/client/src/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AboutPage = () => {
-  return <div>About Page Content</div>;
-};
-
-export default AboutPage;
--- Vocal-Cart/api_d/client/src/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ContactPage = () => {
-  return <div>Contact Page Content</div>;
-};
-
-export default ContactPage;
